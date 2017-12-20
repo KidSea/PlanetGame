09cf71e4f020149b2da9a7a9f7978559016f7041
yuxuehai <yuxuehai@meizu.com>
2017-12-20 09:43:40 +0800

    完成飞船移动

--- src/game/game_functions.py
+++ src/game/game_functions.py
@@ -11,8 +11,19 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
               #向右移动飞船
-              ship.rect.centerx += 1
+              ship.moving_right = True
+
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+              if event.key == pygame.K_RIGHT:
+                  ship.moving_right = False
+
+              elif event.key == pygame.K_LEFT:
+                  ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
 
